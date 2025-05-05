b1ae8786ba67d45984aacdf34c432ae9e7120a09
Femmy :3 <118221254+MFrly@users.noreply.github.com>
2025-05-05 12:46:50 +0200

    vytvoření migrací a super usera

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, WorkoutPlan ,WorkoutExercise, Exercise
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(WorkoutPlan)
+admin.site.register(WorkoutExercise)
+admin.site.register(Exercise)
--- library/models.py
+++ library/models.py
@@ -1,58 +1,61 @@
-import datetime
-
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserProfile(models.Model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    age = models.IntegerField(max_length=2, verbose_name='Age')
-    height = models.FloatField(max_length=3, verbose_name='Height (cm)', error_messages={'max_length': 'Height must be in cm'})
-    weight = models.FloatField(max_length=3, verbose_name='Weight (kg)', error_messages={'max_length': 'Weight must be in kg'})
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    age = models.IntegerField(verbose_name='Age')
+    height = models.FloatField(verbose_name='Height (cm)')
+    weight = models.FloatField(verbose_name='Weight (kg)')
+
     class Meta:
-        ordering = ['name']
+        ordering = ['user__username']
         verbose_name = 'User Profile'
         verbose_name_plural = 'User Profiles'
-        
+
     def __str__(self):
-        return self.name
-    
+        return self.user.username
+
+
 class WorkoutPlan(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='workout_plans')
     planName = models.CharField(max_length=100, verbose_name='Plan Name')
-    finalWeight = models.FloatField(max_length=3, verbose_name='Final Weight (kg)', error_messages={'max_length': 'Weight must be in kg'})
+    finalWeight = models.FloatField(verbose_name='Final Weight (kg)')
     planDescription = models.TextField(blank=True, verbose_name='Plan Description')
-    
+
     class Meta:
         ordering = ['planName']
         verbose_name = 'Workout Plan'
         verbose_name_plural = 'Workout Plans'
-        
+
     def __str__(self):
         return self.planName
-    
-class AllExercises(models.Model):
-    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE, related_name='exercises')
-    sets = models.IntegerField(help_text='Number of sets')
-    reps = models.IntegerField(help_text='Number of reps')
-    
-    class Meta:
-        ordering = ['name']
-        verbose_name = 'Exercise'
-        verbose_name_plural = 'Exercises'
-        
-    def __str__(self):
-        return self.name
-    
+
+
 class Exercise(models.Model):
     name = models.CharField(max_length=100, verbose_name='Exercise Name')
     description = models.TextField(blank=True, verbose_name='Exercise Description')
     gif = models.ImageField(upload_to='exercise_gifs/', blank=True, verbose_name='Exercise GIF')
-    
+
     class Meta:
         ordering = ['name']
         verbose_name = 'Exercise'
         verbose_name_plural = 'Exercises'
-        
+
     def __str__(self):
         return self.name
+
+
+class WorkoutExercise(models.Model):
+    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE, related_name='workout_exercises')
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, related_name='workout_usages')
+    sets = models.IntegerField(help_text='Number of sets')
+    reps = models.IntegerField(help_text='Number of reps')
+
+    class Meta:
+        ordering = ['exercise__name']
+        verbose_name = 'Workout Exercise'
+        verbose_name_plural = 'Workout Exercises'
+
+    def __str__(self):
+        return f"{self.exercise.name} - {self.sets}x{self.reps}"
